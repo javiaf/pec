2bb253cea0c37f3120cb89146659b579685de5eb
Javier Arguello <javiaf@gmail.com>
2016-04-12 18:03:45 +0200

    Initial Commit

---
+++ practica1Tests/practica1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  practica1Tests.swift
+//  practica1Tests
+//
+//  Created by Javier Arguello on 12/4/16.
+//  Copyright © 2016 uoc. All rights reserved.
+//
+
+import XCTest
+@testable import practica1
+
+class practica1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
